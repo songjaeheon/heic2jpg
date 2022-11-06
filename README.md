2cc4fc4b42b9cdb742b83406d84523b5bcdc27da
SONGJAEHEON <spes2you@gmail.com>
2022-11-06 17:13:41 +0900

    git init

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # heic2jpg
 Transform heic images to jpg formatted files.
+
+## Environment Setting
+- OS: Linux
+
+- need to install Pillow library
+
+        `python3 -m pip install --upgrade pip`
+        `python3 -m pip install --upgrade Pillow`
+
+- need to install pyheif library
+
+        `pip install --upgrade pip`
+        `pip install pyheif`
+
+## How to Use
+
+copy `main.py` to the folder that contains heic images.
+
+run the `main.py`.
---
+++ main.py
@@ -0,0 +1,40 @@
+import platform
+import os
+from PIL import Image
+# This code is based on the pyheif ver. 0.7.0
+import pyheif
+
+# These are supported heif extensions by the library. (https://github.com/carsales/pyheif/blob/master/libheif/heif.h)
+heifExtensions = ["heic", "heix", "hevc", "heim", "heis", "hevm", "hevs", "mif1", "msf1", "avif", "avis"]
+
+def main():
+    osCat = platform.system()
+    files = os.listdir(os.getcwd())
+    splitPath = []
+    heicFiles = []
+    for file in files:
+        if(osCat.lower() == "windows"):     # Windows
+            splitPath = file.split('\\')
+        else:   # Mac or Linux
+            splitPath = file.split('/')
+        if(splitPath[-1].split('.')[-1].lower() in heifExtensions):     # File name. (ex. img.jpg)
+            heicFiles.append(file)
+    print(heicFiles)
+    for file in heicFiles:
+        heif_file = pyheif.read(file)
+        image = Image.frombytes(
+            heif_file.mode, 
+            heif_file.size, 
+            heif_file.data,
+            "raw",
+            heif_file.mode,
+            heif_file.stride,
+        )
+        splitPath = file.split('.')
+        splitPath[-1] = ".jpg"
+        savePath = ''.join(splitPath)
+        image.save(savePath, "JPEG")
+        print(savePath + "saved!")
+
+if name == "main":
+    main()
